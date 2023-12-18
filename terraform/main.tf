2ac56823eeda1489c333df41490d8ce1aa467fa7
Angelo Beleno <trainee.angelo.beleno@sigmalabs.co.uk>
2023-12-18 17:25:15 +0000

    terraformed ecr repos

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,14 @@ provider "aws" {
 }
 
 # make ECR repositories for the images
-resource "aws_ecr_repository" "service" {
-  name = "c9-angelo-plant-pipeline"
+resource "aws_ecr_repository" "plant_pipeline" {
+  name = "c9-angelo-plant-pipeline-repo"
+}
+
+resource "aws_ecr_repository" "rds_pipeline" {
+  name = "c9-angelo-rds-s3-pipeline-repo"
+}
+
+resource "aws_ecr_repository" "dashboard" {
+  name = "c9-angelo-dashboard-repo"
 }
