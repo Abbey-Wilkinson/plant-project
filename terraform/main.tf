659ec652dc0f6e2b6586e0d847c76841c3d6d399
Angelo Beleno <trainee.angelo.beleno@sigmalabs.co.uk>
2023-12-19 10:20:32 +0000

    added a task definition for the dash board

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,10 @@ provider "aws" {
     region = "eu-west-2"
 }
 
+data "aws_iam_role" "execution-role" {
+    name = "ecsTaskExecutionRole"
+}
+
 # make ECR repositories for the images
 resource "aws_ecr_repository" "plant_pipeline" {
   name = "c9-angelo-plant-pipeline-repo"
@@ -13,4 +17,34 @@ resource "aws_ecr_repository" "rds_pipeline" {
 
 resource "aws_ecr_repository" "dashboard" {
   name = "c9-angelo-dashboard-repo"
+}
+
+# task definition for dashboard
+# TODO: change the image string to the appropriate image in the ECR
+# Also, configure the environment properly
+resource "aws_ecs_task_definition" "dashboard-task-def" {
+    family = "c9-angelo-dashboard-taskdef"
+    network_mode = "awsvpc"
+    requires_compatibilities = ["FARGATE"]
+    container_definitions = jsonencode([
+        {
+            name: "dashboard"
+            image: "129033205317.dkr.ecr.eu-west-2.amazonaws.com/c9-angelo-dashboard-repo:dummy"
+            essential: true
+            portMappings: [{
+                containerPort = 8501
+                hostPort = 8501
+            }]
+            environment: [
+                { name: "DB_NAME", value: var.DB_NAME },
+                { name: "DB_HOST", value: var.DB_HOST },
+                { name: "DB_PASSWORD", value: var.DB_PASSWORD },
+                { name: "DB_USERNAME", value: var.DB_USERNAME },
+                { name: "DB_PORT", value: var.DB_PORT }
+            ]
+        }
+    ])
+    execution_role_arn = data.aws_iam_role.execution-role.arn
+    memory = 2048
+    cpu = 1024
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,31 @@
+variable "DB_NAME" {
+    type = string
+}
+
+variable "DB_PORT" {
+    type = string
+}
+
+variable "DB_HOST" {
+    type = string
+}
+
+variable "DB_USERNAME" {
+    type = string
+}
+
+variable "DB_PASSWORD" {
+    type = string
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+    type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+    type = string
+}
+
+variable "BUCKET_NAME" {
+    type = string
+}
