fd58827ec80e8e1eb88c3c64d0a3b8ba690f3273
caithomas6789 <caigsthomas@gmail.com>
2023-12-20 15:06:08 +0000

    Added more extract tests

--- pipeline/test_extract.py
+++ pipeline/test_extract.py
@@ -7,39 +7,71 @@ from extract import get_number_of_plants, connect_to_plant_ids
 from errors import APIError
 
 
-def test_connect_to_plant_ids(requests_mock):
-    """tests loads correct weather description"""
+class TestConnectToPlantIDs:
 
-    api_plants = "https://data-eng-plants-api.herokuapp.com/plants/"
-    total_num_plants = 0
-    requests_mock.get(
-        "https://data-eng-plants-api.herokuapp.com/plants/0", status_code=200)
-    response = connect_to_plant_ids(total_num_plants, api_plants)
+    def test_connect_to_plant_ids(self, requests_mock):
+        """tests loads correct weather description"""
 
-    assert response == []
+        api_plants = "https://data-eng-plants-api.herokuapp.com/plants/"
+        total_num_plants = 0
+        requests_mock.get(
+            "https://data-eng-plants-api.herokuapp.com/plants/0", status_code=200)
+        response = connect_to_plant_ids(total_num_plants, api_plants)
+
+        assert response == []
+
+    def test_connect_to_plant_ids_raise_404_error(self, requests_mock):
+
+        api_plants = "https://data-eng-plants-api.herokuapp.com/plants/"
+        total_num_plants = 2
+        requests_mock.get(
+            "https://data-eng-plants-api.herokuapp.com/plants/1", status_code=404)
+        with pytest.raises(APIError) as exception:
+            connect_to_plant_ids(total_num_plants, api_plants)
+
+        assert exception.value.message == {
+            'error': True, 'message': 'URL invalid.'}
+
+    def test_connect_to_plant_ids_raise_400_error(self, requests_mock):
+
+        api_plants = "https://data-eng-plants-api.herokuapp.com/plants/"
+        total_num_plants = 2
+        requests_mock.get(
+            "https://data-eng-plants-api.herokuapp.com/plants/1", status_code=400)
+        with pytest.raises(APIError) as exception:
+            connect_to_plant_ids(total_num_plants, api_plants)
+
+        assert exception.value.message == {
+            'error': True, 'message': 'URL invalid.'}
 
 
-def test_connect_to_plant_ids_raise_404_error(requests_mock):
+class TestGetNumberOfPlants:
+    def test_get_number_of_plants(self, requests_mock):
+        """tests loads correct weather description"""
 
-    api_plants = "https://data-eng-plants-api.herokuapp.com/plants/"
-    total_num_plants = 2
-    requests_mock.get(
-        "https://data-eng-plants-api.herokuapp.com/plants/1", status_code=404)
-    requests_mock.get(
-        "https://data-eng-plants-api.herokuapp.com/plants/2", status_code=404)
-    with pytest.raises(APIError) as exception:
-        connect_to_plant_ids(total_num_plants, api_plants)
+        api_index = "https://data-eng-plants-api.herokuapp.com/"
+        requests_mock.get(
+            "https://data-eng-plants-api.herokuapp.com/", status_code=200, json={'plants_on_display': 49, 'success': 'Liverpool Natural History Museum - Plants API is running'})
+        response = get_number_of_plants(api_index)
 
-    assert exception.value.message == {
-        'error': True, 'message': 'URL invalid.'}
+        assert response == 51
 
+    def test_get_number_of_plants_raise_404_error(self, requests_mock):
+        api_index = "https://data-eng-plants-api.herokuapp.com/"
+        requests_mock.get(
+            "https://data-eng-plants-api.herokuapp.com/", status_code=404, json={})
+        with pytest.raises(APIError) as exception:
+            get_number_of_plants(api_index)
 
-def test_get_number_of_plants(requests_mock):
-    """tests loads correct weather description"""
+        assert exception.value.message == {
+            'error': True, 'message': "No key 'plants_on_display' found."}
 
-    api_index = "https://data-eng-plants-api.herokuapp.com/"
-    requests_mock.get(
-        "https://data-eng-plants-api.herokuapp.com/", status_code=404)
-    response = get_number_of_plants(api_index)
+    def test_get_number_of_plants_raise_400_error(self, requests_mock):
+        api_index = "https://data-eng-plants-api.herokuapp.com/"
+        requests_mock.get(
+            "https://data-eng-plants-api.herokuapp.com/", status_code=400, json={})
+        with pytest.raises(APIError) as exception:
+            get_number_of_plants(api_index)
 
-    assert response == []
+        assert exception.value.message == {
+            'error': True, 'message': "No key 'plants_on_display' found."}
