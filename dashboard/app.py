4dfc680059e17a8b695e78eb1b2930eb4d5260bb
Abbey Wilkinson <trainee.abbey.wilkinson@sigmalabs.co.uk>
2023-12-20 09:17:50 +0000

    added average soil moisture

--- dashboard/app.py
+++ dashboard/app.py
@@ -7,6 +7,9 @@ from dotenv import load_dotenv
 import pandas as pd
 import streamlit as st
 
+from database import get_database_connection, load_all_plant_data
+from utilities import get_average_soil_moisture
+
 
 def get_header_metrics() -> None:
     """Gets the main headers and displays them."""
@@ -15,20 +18,23 @@ def get_header_metrics() -> None:
         st.metric("Total Number of Plants :herb:",
                   len(selected_plants))
     with head_cols[1]:
-        st.metric("Average Soil Moisture: :potted_plant:", )
+        st.metric("Average Soil Moisture: :potted_plant:",
+                  get_average_soil_moisture(plants))
 
 
 if __name__ == "__main__":
 
     load_dotenv()
 
-    plants = pd.read_csv("./../pipeline/transformed_test.csv")
+    conn = get_database_connection()
+
+    plants = load_all_plant_data(conn)
 
     st.title("Plant Sensors Dashboard")
 
     selected_plants = st.sidebar.multiselect("Selected Plants",
-                                             plants["plant_id"].unique(),
-                                             default=plants["plant_id"].unique())
+                                             plants["plant_name"].unique(),
+                                             default=plants["plant_name"].unique())
 
     if selected_plants:
 
