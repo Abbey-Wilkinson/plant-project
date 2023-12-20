f8464fbc5c93d1edd0046908e98743bcc2ddb0f1
Abbey Wilkinson <trainee.abbey.wilkinson@sigmalabs.co.uk>
2023-12-20 11:18:52 +0000

    Made visualisations script

---
+++ dashboard/visualisations.py
@@ -0,0 +1,42 @@
+"""
+Functions to visualise data on streamlit.
+"""
+import altair as alt
+import pandas as pd
+from pandas import DataFrame
+
+
+# [TODO]: Create bar chart for below function.
+
+def get_latest_temperature_readings(plants: DataFrame):
+    """
+    Returns an altair bar chart that shows the latest temperature readings for each plant.
+    """
+    pass
+
+
+# [TODO]: Create bar chart for below function.
+
+def get_latest_soil_moisture_readings(plants: DataFrame):
+    """
+    Returns an altair bar chart that shows the latest soil moisture readings for each plant.
+    """
+    pass
+
+
+# [TODO]: Create line chart for below function.
+
+def get_temperature_over_time(plants: DataFrame):
+    """
+    Returns an altair line chart that shows the temperature readings for each plant over time.
+    """
+    pass
+
+
+# [TODO]: Create line chart for below function.
+
+def get_soil_moisture_over_time(plants: DataFrame):
+    """
+    Returns an altair line chart that shows the soil moisture readings for each plant over time.
+    """
+    pass
