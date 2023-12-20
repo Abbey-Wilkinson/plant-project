acda885413722bdf45110bc43abd5dc5dbacee4d
Abbey Wilkinson <trainee.abbey.wilkinson@sigmalabs.co.uk>
2023-12-20 09:19:37 +0000

    Created utility script for functions in main app

---
+++ dashboard/utilities.py
@@ -0,0 +1,29 @@
+"""
+Utility functions for the streamlit app.
+"""
+from dotenv import load_dotenv
+import numpy
+import pandas as pd
+
+
+from database import get_database_connection, load_all_plant_data
+
+
+def get_average_soil_moisture(df: pd.DataFrame) -> numpy.float64:
+    """
+    Returns the average soil moisture from all of the plants combined.
+    """
+    return df["soil_moisture"].mean().round(2)
+
+
+if __name__ == "__main__":
+
+    load_dotenv()
+
+    conn = get_database_connection()
+
+    plants = load_all_plant_data(conn)
+
+    average_soil_moisture = get_average_soil_moisture(plants)
+
+    print(type(average_soil_moisture))
