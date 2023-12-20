039d90725223801d9be0141bba05c87403efc399
Oscar <trainee.oscar.borden@sigmalabs.co.uk>
2023-12-20 12:28:50 +0000

    Linted transform

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -1,8 +1,10 @@
 """
 Transform script for the plants pipeline.
 """
-import pandas as pd
+
 from time import perf_counter
+import pandas as pd
+
 
 MINIMUM_SOIL_MOISTURE = 0
 MAXIMUM_SOIL_MOISTURE = 100
@@ -10,63 +12,68 @@ MINIMUM_TEMPERATURE = 5
 MAXIMUM_TEMPERATURE = 50
 
 
-def clean_recording_taken_data(df: pd.DataFrame) -> pd.DataFrame:
+def clean_recording_taken_data(plants_df: pd.DataFrame) -> pd.DataFrame:
     """
     Cleans the recording taken column from the DataFrame and
     drops any NaN values once values converted to datetimes.
     """
 
-    df["recording_taken_bool"] = pd.to_datetime(
-        df["recording_taken"], errors="coerce").notna()
-    df = df.drop(df[df.recording_taken_bool == False].index)
+    plants_df["recording_taken_bool"] = pd.to_datetime(
+        plants_df["recording_taken"], errors="coerce").notna()
+    plants_df = plants_df.drop(
+        plants_df[plants_df.recording_taken_bool is False].index)
 
-    df = df.drop(columns=["recording_taken_bool"])
+    plants_df = plants_df.drop(columns=["recording_taken_bool"])
 
-    return df
+    return plants_df
 
 
-def clean_last_watered_data(df: pd.DataFrame) -> pd.DataFrame:
+def clean_last_watered_data(plants_df: pd.DataFrame) -> pd.DataFrame:
     """
     Cleans the last watered column from the DataFrame and
     drops any NaN values once values converted to datetimes.
     """
 
-    df["last_watered"] = pd.to_datetime(
-        df["last_watered"], errors="coerce")
+    plants_df["last_watered"] = pd.to_datetime(
+        plants_df["last_watered"], errors="coerce")
 
-    df = df.dropna()
+    plants_df = plants_df.dropna()
 
-    return df
+    return plants_df
 
 
-def clean_soil_moisture_data(df: pd.DataFrame) -> pd.DataFrame:
+def clean_soil_moisture_data(plants_df: pd.DataFrame) -> pd.DataFrame:
     """
     Clears all invalid soil moistures from the DataFrame.
     """
 
-    df["soil_moisture"] = pd.to_numeric(
-        df["soil_moisture"], errors="coerce")
-    df = df.drop(df[df.soil_moisture < MINIMUM_SOIL_MOISTURE].index)
-    df = df.drop(df[df.soil_moisture > MAXIMUM_SOIL_MOISTURE].index)
+    plants_df["soil_moisture"] = pd.to_numeric(
+        plants_df["soil_moisture"], errors="coerce")
+    plants_df = plants_df.drop(
+        plants_df[plants_df.soil_moisture < MINIMUM_SOIL_MOISTURE].index)
+    plants_df = plants_df.drop(
+        plants_df[plants_df.soil_moisture > MAXIMUM_SOIL_MOISTURE].index)
 
-    df = df.dropna()
+    plants_df = plants_df.dropna()
 
-    return df
+    return plants_df
 
 
-def clean_temperature_data(df: pd.DataFrame) -> pd.DataFrame:
+def clean_temperature_data(plants_df: pd.DataFrame) -> pd.DataFrame:
     """
     Clears all invalid temperatures from the DataFrame.
     """
 
-    df["temperature"] = pd.to_numeric(
-        df["temperature"], errors="coerce")
-    df = df.drop(df[df.temperature < MINIMUM_TEMPERATURE].index)
-    df = df.drop(df[df.temperature > MAXIMUM_TEMPERATURE].index)
+    plants_df["temperature"] = pd.to_numeric(
+        plants_df["temperature"], errors="coerce")
+    plants_df = plants_df.drop(
+        plants_df[plants_df.temperature < MINIMUM_TEMPERATURE].index)
+    plants_df = plants_df.drop(
+        plants_df[plants_df.temperature > MAXIMUM_TEMPERATURE].index)
 
-    df = df.dropna()
+    plants_df = plants_df.dropna()
 
-    return df
+    return plants_df
 
 
 if __name__ == "__main__":
