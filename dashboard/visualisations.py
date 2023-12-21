c879a6de8ef9dbbed61aff7d39efba6e55e61887
Abbey Wilkinson <trainee.abbey.wilkinson@sigmalabs.co.uk>
2023-12-21 09:06:46 +0000

    small change

--- dashboard/visualisations.py
+++ dashboard/visualisations.py
@@ -16,17 +16,17 @@ def get_latest_temperature_readings(plants: DataFrame, sort_ascending):
     latest_data = get_latest_data(plants)
 
     latest_data["Plant Name"] = latest_data[["plant_name"]]
-    latest_data["Temperature"] = latest_data[["temperature"]]
+    latest_data["Temperature (°C)"] = latest_data[["temperature"]]
 
-    latest_data = latest_data[["Plant Name", "Temperature"]]
+    latest_data = latest_data[["Plant Name", "Temperature (°C)"]]
     print(latest_data)
 
     sort_order = "x" if sort_ascending else "-x"
 
     latest_temp_readings = alt.Chart(latest_data).mark_bar().encode(
         y=alt.Y('Plant Name:N', sort=sort_order),
-        x=alt.X('Temperature:Q'),
-        tooltip=['Plant Name:N', 'Temperature:Q'],
+        x=alt.X('Temperature (°C):Q'),
+        tooltip=['Plant Name:N', 'Temperature (°C):Q'],
         color=alt.Color('Plant Name:N', legend=None).scale(scheme='greens')
     ).properties(
         title='Latest Temperature of Plants',
@@ -42,17 +42,18 @@ def get_latest_soil_moisture_readings(plants: DataFrame, sort_ascending):
     latest_data = get_latest_data(plants)
 
     latest_data["Plant Name"] = latest_data[["plant_name"]]
-    latest_data["Soil Moisture Percentage"] = latest_data[["soil_moisture"]]
+    latest_data["Soil Moisture (%)"] = latest_data[[
+        "soil_moisture"]]
 
-    latest_data = latest_data[["Plant Name", "Soil Moisture Percentage"]]
+    latest_data = latest_data[["Plant Name", "Soil Moisture (%)"]]
     print(latest_data)
 
     sort_order = "x" if sort_ascending else "-x"
 
     latest_temp_readings = alt.Chart(latest_data).mark_bar().encode(
         y=alt.Y('Plant Name:N', sort=sort_order),
-        x=alt.X('Soil Moisture Percentage:Q'),
-        tooltip=['Plant Name:N', 'Soil Moisture Percentage:Q'],
+        x=alt.X('Soil Moisture (%):Q'),
+        tooltip=['Plant Name:N', 'Soil Moisture (%):Q'],
         color=alt.Color('Plant Name:N', legend=None).scale(scheme='browns')
     ).properties(
         title='Latest Soil Moisture Percentage of Plants',
